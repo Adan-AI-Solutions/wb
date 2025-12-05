92a3550ab65f8f79f29431664ef7fc018baf6437
mutt5 <yakigao5@gmail.com>
2025-12-05 15:58:55 +0900

    chore: Firebase設定にmeasurementIdを追加

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET?: string
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID?: string
   readonly VITE_FIREBASE_APP_ID?: string
+  readonly VITE_FIREBASE_MEASUREMENT_ID?: string
   readonly VITE_USE_EMULATOR?: string
   readonly VITE_API_TODOS_LIST_ENDPOINT?: string
   readonly VITE_API_TODOS_GET_ENDPOINT?: string
